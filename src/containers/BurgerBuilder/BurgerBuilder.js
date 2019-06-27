8c610b2c63cd62744cbe14c13f8661bdd0d66792
SimranMakhija7 <simran.makhija2@gmail.com>
2019-06-27 12:39:57 +0530

    folder structure changes

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Burger from '../../components/Burger/Burger'
+import Aux from '../../hoc/Auxiliary'
+class BurgerBuilder extends React.Component{
+    // constructor(props){
+    //     super(props);
+    //     this.state={}
+    // }
+
+    state={
+        ingredients:{
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:2
+        }
+    }
+
+    render(){
+        return(
+            <Aux>
+               <Burger ingredients={this.state.ingredients}/>
+               <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
